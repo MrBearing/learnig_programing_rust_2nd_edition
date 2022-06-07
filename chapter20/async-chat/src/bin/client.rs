7707ccd3a0fdce77b86babb70ed76734882539ee
MrBearing <takumi1988okamoto@gmail.com>
2022-06-07 21:12:15 +0900

    update

--- chapter20/async-chat/src/bin/client.rs
+++ chapter20/async-chat/src/bin/client.rs
@@ -3,6 +3,11 @@ use async_chat::utils::{self,ChatResult};
 use async_std::io;
 use async_std::net;
 
+fn parse_command(command :&String) -> Option<String>{
+    // TODO この部分はGithubを見ながら実装
+    None
+}
+
 async fn send_commands(mut to_server: net::TcpStream) -> ChatResult<()>{
     
     println!("Commands:\n\
@@ -12,7 +17,12 @@ async fn send_commands(mut to_server: net::TcpStream) -> ChatResult<()>{
             to close the connection.");
     let mut command_lines = io::BufReader::new(io::stdin()).lines();
     while let Some(command_result) = command_lines.next().await {
-
+        let command = command_result?;
+        let request = match parse_command(&command){
+            Some(request) => request,
+            None => continue,
+        };
+        to_server.flush().await?;
     }
     Ok(())
 }
